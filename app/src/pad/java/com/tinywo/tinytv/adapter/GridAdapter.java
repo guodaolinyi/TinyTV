cf17e7488f8edb2408ad1d179862e696c0115a0c
锅岛霖懿 <1397871454@qq.com>
2026-02-22 17:00:12 +0800

    feat: 添加多个新功能组件和适配器
    新增HttpUrlUtil工具类、多个适配器(如PinyinAdapter、GridFilterKVAdapter等)和自定义控件(如MarqueeTextView、NoScrollViewPager等)
    优化布局文件中的控件引用路径
    重构gradle配置合并不同源集的资源
    新增多个对话框(如AboutDialog、TipDialog等)
    添加视频文件夹管理相关功能
    新增订阅管理功能
    完善直播相关功能组件

---
+++ app/src/pad/java/com/tinywo/tinytv/adapter/GridAdapter.java
@@ -0,0 +1,64 @@
+package com.tinywo.tinytv.adapter;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.tinywo.tinytv.R;
+import com.tinywo.tinytv.bean.Movie;
+import com.tinywo.tinytv.picasso.RoundTransformation;
+import com.tinywo.tinytv.util.DefaultConfig;
+import com.tinywo.tinytv.util.MD5;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import me.jessyan.autosize.utils.AutoSizeUtils;
+
+/**
+ * @author pj567
+ * @date :2020/12/21
+ * @description:
+ */
+public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
+
+    public GridAdapter() {
+        super(R.layout.item_grid, new ArrayList<>());
+
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Movie.Video item) {
+
+        TextView tvYear = helper.getView(R.id.tvYear);
+        if (item.year <= 0) {
+            tvYear.setVisibility(View.GONE);
+        } else {
+            tvYear.setText(String.valueOf(item.year));
+            tvYear.setVisibility(View.VISIBLE);
+        }
+
+        if (TextUtils.isEmpty(item.note)) {
+            helper.setVisible(R.id.tvNote, false);
+        } else {
+            helper.setVisible(R.id.tvNote, true);
+            helper.setText(R.id.tvNote, item.note);
+        }
+        helper.setText(R.id.tvName, item.name);
+        ImageView ivThumb = helper.getView(R.id.ivThumb);
+        //由于部分电视机使用glide报错
+        if (!TextUtils.isEmpty(item.pic)) {
+            item.pic=item.pic.trim();
+            Picasso.get()
+                    .load(DefaultConfig.checkReplaceProxy(item.pic))
+                    .placeholder(R.drawable.img_loading_placeholder)
+                    .error(R.drawable.img_loading_placeholder)
+                    .into(ivThumb);
+        } else {
+            ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+        }
+    }
+}
---
+++ app/src/pad/java/com/tinywo/tinytv/dialog/AllChannelsRightDialog.java
@@ -0,0 +1,72 @@
+package com.tinywo.tinytv.dialog;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.blankj.utilcode.util.ColorUtils;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.tinywo.tinytv.R;
+import com.tinywo.tinytv.bean.VodInfo;
+import com.tinywo.tinytv.databinding.DialogAllChannelBinding;
+import com.tinywo.tinytv.activity.LiveActivity;
+import com.tinywo.tinytv.adapter.LiveChannelGroupNewAdapter;
+import com.tinywo.tinytv.adapter.LiveChannelItemNewAdapter;
+import com.tinywo.tinytv.widget.GridSpacingItemDecoration;
+import com.tinywo.tinytv.util.FastClickCheckUtil;
+import com.tinywo.tinytv.util.Utils;
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopup.core.DrawerPopupView;
+import com.lxj.xpopup.enums.PopupPosition;
+import com.lxj.xpopup.interfaces.OnSelectListener;
+import com.owen.tvrecyclerview.widget.V7LinearLayoutManager;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class AllChannelsRightDialog extends DrawerPopupView {
+
+    private final LiveActivity mActivity;
+    private com.tinywo.tinytv.databinding.DialogAllChannelBinding mBinding;
+
+    public AllChannelsRightDialog(@NonNull @NotNull Context context) {
+        super(context);
+        mActivity = (LiveActivity) context;
+    }
+
+    @Override
+    protected int getImplLayoutId() {
+        return R.layout.dialog_all_channel;
+    }
+
+    @Override
+    protected void onCreate() {
+        super.onCreate();
+        mBinding = DialogAllChannelBinding.bind(getPopupImplView());
+        initChannelGroupView();
+        initLiveChannelView();
+    }
+
+    private void initChannelGroupView() {
+        mBinding.mGroupGridView.setHasFixedSize(true);
+        mBinding.mGroupGridView.setLayoutManager(new V7LinearLayoutManager(getContext(), 1, false));
+
+        if (mActivity.liveChannelGroupAdapter!=null){
+            mBinding.mGroupGridView.setAdapter(mActivity.liveChannelGroupAdapter);
+        }
+
+    }
+    private void initLiveChannelView() {
+        mBinding.mChannelGridView.setHasFixedSize(true);
+        mBinding.mChannelGridView.setLayoutManager(new V7LinearLayoutManager(getContext(), 1, false));
+
+        if (mActivity.liveChannelItemAdapter!=null){
+            mBinding.mChannelGridView.setAdapter(mActivity.liveChannelItemAdapter);
+        }
+    }
+
+}
